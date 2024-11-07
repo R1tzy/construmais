a380451aa216bc399a62646bb5975b7b0d535592
Thiago Lopes <46581137+R1tzy@users.noreply.github.com>
2024-11-07 15:56:33 -0300

    Update database.php

--- construmais/database.php
+++ construmais/database.php
@@ -1,10 +1,11 @@
 <?php
 
-$hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "construmais";
-
+// Configurações do banco de dados
+$hostName = "localhost"; // Servidor do banco de dados
+$dbUser = "root"; // Usuário do MySQL
+$dbPassword = ""; // Senha do MySQL (deixe em branco se não houver senha)
+$dbName = "construmais"; // Nome do banco de dados
+ 
 try{
     $conn = new PDO("mysql:host=$hostName;dbname=$dbName", $dbUser, $dbPassword);
     // Configure o PDO para lançar exceções em caso de erro
